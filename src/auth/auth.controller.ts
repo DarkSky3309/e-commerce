6044948cca881c5edaf9102bf20621b033c7f4de
DarkSky3309 <lust3309@gmail.com>
2023-05-08 20:05:23 +0300

    auth done

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,7 +7,8 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthDto } from './auth.dto';
+import { AuthDto } from './dto/auth.dto';
+import { TokenDto } from './dto/token.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -23,12 +24,12 @@ export class AuthController {
   @HttpCode(200)
   @Post('login')
   async login(@Body() dto: AuthDto) {
-    return this.authService.register(dto);
+    return this.authService.login(dto);
   }
   @UsePipes(new ValidationPipe())
   @HttpCode(200)
   @Post('login/access-token')
-  async getNewToken(@Body() dto: AuthDto) {
-    return this.authService.register(dto);
+  async getNewToken(@Body() dto: TokenDto) {
+    return this.authService.getNewTokens(dto.refreshToken);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,12 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable, NotFoundException,
+  UnauthorizedException
+} from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
-import { AuthDto } from './auth.dto';
+import { AuthDto } from './dto/auth.dto';
 import { faker } from '@faker-js/faker';
-import { hash } from 'argon2';
+import { hash, verify } from 'argon2';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '@prisma/client';
 
@@ -12,6 +16,28 @@ export class AuthService {
     private readonly prismaService: PrismaService,
     private readonly jwtService: JwtService
   ) {}
+
+  async getNewTokens(refreshToken: string) {
+    const result = await this.jwtService.verifyAsync(refreshToken);
+    if (!result) throw new UnauthorizedException('Invalid token');
+    const user = await this.prismaService.user.findUnique({
+      where: { id: result.id },
+    });
+    const tokens = await this.generateToken(user.id);
+    return {
+      user: this.returnUserFields(user),
+      ...tokens,
+    };
+  }
+
+  async login(dto: AuthDto) {
+    const user = await this.validateUser(dto);
+    const tokens = await this.generateToken(user.id);
+    return {
+      user: this.returnUserFields(user),
+      ...tokens,
+    };
+  }
 
   async register(dto: AuthDto) {
     const oldUser = await this.prismaService.user.findUnique({
@@ -48,10 +74,25 @@ export class AuthService {
     });
     return { accessToken, refreshToken };
   }
+
   private returnUserFields(user: User) {
     return {
       id: user.id,
       email: user.email,
     };
+  }
+
+  private async validateUser(dto: AuthDto) {
+    const user = await this.prismaService.user.findUnique({
+      where: { email: dto.email },
+    });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    const isPasswordValid = await verify(user.password, dto.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid password');
+    }
+    return user;
   }
 }
