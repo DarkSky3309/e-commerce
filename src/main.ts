87756de1b665a25b6e2635a7c5f7a27dc9f96d20
DarkSky3309 <lust3309@gmail.com>
2023-05-08 18:50:42 +0300

    start make auth

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api')
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+  app.setGlobalPrefix('api');
   app.enableCors();
   await app.listen(3000);
 }
+
 bootstrap();
