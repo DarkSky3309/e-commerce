24922d285987310a243071adf6149a2285670d4c
DarkSky3309 <lust3309@gmail.com>
2023-05-14 14:36:03 +0300

    statistic done

--- src/statistics/statistics.service.ts
+++ src/statistics/statistics.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { UserService } from '../user/user.service';
 
 @Injectable()
-export class StatisticsService {}
+export class StatisticsService {
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly userService: UserService
+  ) {
+  }
+
+  async getMainStatistics(userId: number) {
+    const user = await this.userService.getProfile(userId, {
+      orders: {
+        select: {
+          items: {
+            select: {
+              price: true
+            }
+          }
+        }
+      },
+      reviews: true
+    });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return [
+      {
+        name: 'Orders',
+        value: user.orders.length
+      },
+      {
+        name: 'Reviews',
+        value: user.reviews.length
+      },
+      {
+        name: 'Favorite products',
+        value: user.favorits.length
+      },
+      {
+        name: 'Total spent',
+        value: 1000
+      }
+    ];
+  }
+}
